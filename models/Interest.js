d854d52fbc3b27188bb8c40c014f1c1090cad455
ramnath086 <ramnath86@gmail.com>
2025-06-17 00:37:07 +0530

    Create Interest.js

---
+++ models/Interest.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const interestSchema = new mongoose.Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    to: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected'],
+        default: 'pending'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Interest', interestSchema);
