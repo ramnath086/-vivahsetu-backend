cc6e214ab59bbe7e91369a1d3ea5e9e8ae32b629
ramnath086 <ramnath86@gmail.com>
2025-06-16 23:44:44 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profile: {
+        gender: String,
+        dob: Date,
+        height: String,
+        caste: String,
+        subCaste: String,
+        gotra: String,
+        education: String,
+        occupation: String,
+        income: String,
+        location: String,
+        about: String,
+        photo: String
+    },
+    isPremium: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
