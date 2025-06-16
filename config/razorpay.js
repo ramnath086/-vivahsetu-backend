84e04e26a8da472ff886815704fc1f43c3a72e60
ramnath086 <ramnath86@gmail.com>
2025-06-17 01:10:28 +0530

    Update razorpay.js

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,8 +1,10 @@
-const Razorpay = require('razorpay');
+// Temporary solution until Razorpay is needed
+const dummyRazorpay = {
+    orders: {
+        create: async () => {
+            throw new Error('Payment feature coming soon');
+        }
+    }
+};
 
-const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_SECRET
-});
-
-module.exports = razorpay;
+module.exports = dummyRazorpay;
