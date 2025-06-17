8d99b4e0dbb4ce2d76940b6d98c0241f124bd1f8
ramnath086 <ramnath86@gmail.com>
2025-06-17 13:04:16 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-    from: {
+    sender: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
-    to: {
+    receiver: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
